eaa8360066f91159d45d4f613d2b8fa2fe912ed1
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-23 18:09:12 +0100

    sostituzione category tag in address

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -21,7 +21,7 @@ class PostsTableSeeder extends Seeder
             $newPost->title = $faker->sentence(1);
             $newPost->author = $faker->name();
             $newPost->post_content = $faker->sentence(5);
-            // $newPost->category_id = $faker->numberBetween(1, 6);
+            // $newPost->category_id = $faker->numberBetween(1, 7);
             $newPost->img_url = $faker->imageUrl(300, 300, $newPost->title, true);
             $newPost->slug = Str::slug($newPost->title, '-');
             $newPost->save();
