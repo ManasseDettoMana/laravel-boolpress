27b2fc238e5d96c1194f4fb96a36c73f46aea3ca
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-26 00:31:03 +0100

    implementazione vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <h1>contenuto di prova VUE</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, commodi quam facilis earum culpa eaque. Magnam, repudiandae possimus. Voluptatum, laboriosam voluptatibus. Voluptatem vel quo qui atque, omnis non fugiat dolores? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sequi, ex nulla animi sunt dolores consequatur, qui mollitia incidunt nihil magni praesentium laboriosam laudantium repellat veniam labore aliquid cum distinctio.
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, commodi quam facilis earum culpa eaque. Magnam, repudiandae possimus. Voluptatum, laboriosam voluptatibus. Voluptatem vel quo qui atque, omnis non fugiat dolores?</p>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: 'App', 
+       data(){
+            return{
+                variable: ''
+          }
+       },
+       components: {}
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ resources/js/front.js
@@ -0,0 +1,7 @@
+window.Vue = require('vue');
+
+import App from './components/App.vue';
+const app = new Vue({
+    el: '#root',
+    render: h=>h(App)
+});
