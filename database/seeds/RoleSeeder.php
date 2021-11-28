22ebcdb65f715062245c3d31e6853607f4c6e320
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-28 18:32:01 +0100

    roles created

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -11,6 +12,16 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $roleList = ['thaSupreme','Amministratore', 'Editore', 'Redattore', 'Scrittore', 'Lmao', 'Lettore'];
+
+        for($i = 1; $i <= 7; $i++)
+        {
+            $newRole = new Role();
+            $newRole->rank = $i;
+            $newRole->name = $roleList[$i-1];
+
+            $newRole->save();
+        }
+
     }
 }
